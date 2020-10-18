b131001691077dceed6241ded7c4d4a2cb7cd891
ttan0408 <71804399+ttan0408@users.noreply.github.com>
2020-10-17 22:16:41 -0500

    Adding PyPoll.py file.

--- PyPoll.py
+++ PyPoll.py
@@ -21,6 +21,10 @@ total_votes = 0
 candidate_options = []
 # Declare a the empty dictionary
 candidate_votes = {}
+# Winning Candidate and Winning Count Tracker
+winning_candidate = ""
+winning_count = 0
+winning_percentage = 0
 
 #Open the election results and read the file
 with open(file_to_load,"r") as election_data:
@@ -56,8 +60,27 @@ for candidate_name in candidate_votes:
     votes = candidate_votes[candidate_name] 
     # 3. Calculate the percentage of votes
     vote_percentage = float(votes)/float(total_votes)*100
-    # 4. Print the candidate name and percentage votes.
-    print(f"{candidate_name}: received {vote_percentage}% of the vote.")    
+
+    # To do : print out each candidate's name, voter count, and percentage of votes to the terminal
+    print(f"{candidate_name}: {vote_percentage:.1f}% ({votes:,})\n")
+
+    # Determine winning vote count and candidate
+    # Determine if the votes is greater than the winning count.
+    if (votes > winning_count) and (vote_percentage > winning_percentage):
+        #if true then set winning_count = votes and winning_percent = voter_percent.
+        winning_count = votes
+        winning_percentage = vote_percentage
+        #And, set the winning_candidate equal to the candidate's name.
+        winning_candidate = candidate_name
+# To do : print out the winning candidate, vote count and percentage to terminal
+winning_candidate_summary = (f"--------------------------\n"
+                             f"Winner: {winning_candidate}\n"
+                             f"Winning Vote Count: {winning_count:,}\n"
+                             f"Winning Percentage: {winning_percentage:.1f}%\n"
+                             f"--------------------------\n")
+print(winning_candidate_summary)
+    
+        
 
 #Print the candidate list
 #print(candidate_options)
@@ -65,7 +88,8 @@ for candidate_name in candidate_votes:
 
 # 3. Print the total votes.
 #print(total_votes)
-
+# 4 Print voter names and %
+#print(f"{candidate_name}: received {vote_percentage}% of the vote.")
      
 
 
